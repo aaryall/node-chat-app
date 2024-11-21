02559d0cfd580476b32733bf655370ac4c16700b
Aryan <arsharyanyadav@gmail.com>
2024-11-21 22:33:38 +0530

    Second Commit

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-
-
+dotenv.config();
+const url = process.env.DB_URL;
 export const connectToDatabase = async ()=>{
-    await mongoose.connect("mongodb://localhost:27017/chapApp",{
+    await mongoose.connect(url,{
         useNewUrlParser : true,
         useUnifiedTopology : true
     });
